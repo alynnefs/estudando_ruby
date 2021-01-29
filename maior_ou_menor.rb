90375148a86d331fc61e7cc6371e6b91f169a832
alynne <alynneferreiras@gmail.com>
2021-01-28 22:55:40 -0300

    capítulo 2

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -9,10 +9,26 @@ puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts "Escolhido! Que tal adivinhar nosso número secreto?\n"
 
-puts "Tentativa 1"
-puts "Entre com o número"
-chute = gets
+limite_de_tentativas = 5
 
-puts "Será que acertou? Você chutou " + chute
+for tentativa in 1..limite_de_tentativas
+  puts "\n\n"
+  puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+  puts "Entre com o número"
+  chute = gets
+  puts "Será que acertou? Você chutou " + chute
 
-puts 175 == chute.to_i
+  acertou = numero_secreto == chute.to_i
+
+  if acertou
+    puts "Acertou!"
+    break
+  else
+    maior = numero_secreto > chute.to_i
+    if maior
+      puts "O número secreto é maior"
+    else
+      puts "O número secreto é menor"
+    end
+  end
+end
